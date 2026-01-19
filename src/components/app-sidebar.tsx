bd945e602c9c01838c3d6484e3b218b451e4065f
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-01-19 21:23:26 +0600

    dashboard sidebar added and all the routes and the other necessary stuff

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,55 +1,64 @@
-import * as React from "react"
+import * as React from "react";
 
-import { SearchForm } from "@/components/search-form"
-import { VersionSwitcher } from "@/components/version-switcher"
+
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarRail,
-} from "@/components/ui/sidebar"
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import {adminRoutes} from "@/routes/adminRoutes";
+import {userRoutes} from "@/routes/userRoutes";
+import {Route} from "@/types/routes.type";
 
 
-const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
+export function AppSidebar({
+                               user,
+                               ...props
+                           }: {
+    user: { role: string } & React.ComponentProps<typeof Sidebar>;
+}) {
+    let routes: Route[] = [];
+
+    switch (user.role) {
+        case "admin":
+            routes = adminRoutes;
+            break;
+        case "user":
+            routes = userRoutes;
+            break;
+        default:
+            routes = [];
+            break;
     }
 
-  ],
-}
-
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar {...props}>
-      <SidebarContent>
-        {/* We create a SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
-      </SidebarContent>
-      <SidebarRail />
-    </Sidebar>
-  )
+    return (
+        <Sidebar {...props}>
+            <SidebarContent>
+                {routes.map((item) => (
+                    <SidebarGroup key={item.title}>
+                        <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {item.items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link href={item.url}>{item.title}</Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                ))}
+            </SidebarContent>
+            <SidebarRail/>
+        </Sidebar>
+    );
 }
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import { Route } from "@/types/routes.type";
+
+export const userRoutes: Route[] = [
+    {
+        title: "Blog Management",
+        items: [
+            {
+                title: "Create Blog",
+                url: "/create-blog",
+            },
+        ],
+    },
+];
