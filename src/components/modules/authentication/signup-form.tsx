3b03a7468f89eb69138ac1546bc54d0e8b444015
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-01-22 23:07:25 +0600

    user registration and login

--- src/components/modules/authentication/signup-form.tsx
+++ src/components/modules/authentication/signup-form.tsx
@@ -10,11 +10,12 @@ import {
 } from "@/components/ui/card"
 
 
-
-import { useForm} from "@tanstack/react-form";
-import {Field,FieldError, FieldGroup, FieldLabel} from "@/components/ui/field";
+import {useForm} from "@tanstack/react-form";
+import {Field, FieldError, FieldGroup, FieldLabel} from "@/components/ui/field";
 import {Input} from "@/components/ui/input";
 import * as z from "zod";
+import {authClient} from "@/lib/auth-client";
+import { toast } from "sonner";
 
 const formScheme = z.object({
     name: z.string().min(2, {message: "Name must be at least 2 characters"}),
@@ -30,10 +31,23 @@ export function SignupForm({...props}: React.ComponentProps<typeof Card>) {
             email: '',
             password: '',
         },
-        validators:{
-          onSubmit:formScheme,
+        validators: {
+            onSubmit: formScheme,
         },
-        onSubmit: ({value}) => {
+
+        onSubmit: async  ({value}) => {
+            const toastId = toast.loading("creating your account...");
+            try{
+                const {error} = await authClient.signUp.email(value);
+
+                if(error){
+                    toast.error(error.message, {id: toastId});
+                    return;
+                }
+                toast.success("user created successfully!", {id: toastId});
+            }catch (err){
+                toast.error("Something went wrong!", {id: toastId});
+            }
             console.log('Form submitted with values:', value);
         }
     })
@@ -71,7 +85,7 @@ export function SignupForm({...props}: React.ComponentProps<typeof Card>) {
                                             value={field.state.value}
                                             onChange={(e) => field.handleChange(e.target.value)}
                                         />
-                                        {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                        {isInvalid && <FieldError errors={field.state.meta.errors}/>}
                                     </Field>
                                 );
                             }}
@@ -93,7 +107,7 @@ export function SignupForm({...props}: React.ComponentProps<typeof Card>) {
                                             value={field.state.value}
                                             onChange={(e) => field.handleChange(e.target.value)}
                                         />
-                                        {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                        {isInvalid && <FieldError errors={field.state.meta.errors}/>}
                                     </Field>
                                 );
                             }}
@@ -116,7 +130,7 @@ export function SignupForm({...props}: React.ComponentProps<typeof Card>) {
                                         value={field.state.value}
                                         onChange={(e) => field.handleChange(e.target.value)}
                                     />
-                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                    {isInvalid && <FieldError errors={field.state.meta.errors}/>}
                                 </Field>
                             );
                         }}
