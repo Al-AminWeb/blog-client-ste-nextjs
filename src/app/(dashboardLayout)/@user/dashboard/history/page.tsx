9e7b98f84d9ce39a10fff57c3a22724cf1fdf34b
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-01-25 22:03:03 +0600

    feat: add pagination controls to HistoryPage and update blog post retrieval logic

--- src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import {blogService} from "@/services/blog.service";
+import { blogService } from "@/services/blog.service";
 import HistoryTable from "@/components/modules/user/histotyTable/HistoryTable";
+import PaginationControls from "@/components/ui/pagination-controls";
 
-export default async function HistoryPage() {
-    const response = await blogService.getBlogPosts({page:"2"});
-
-    const posts = response.data?.data;
+export default async function HistoryPage({ searchParams }: { searchParams: { page?: string } }) {
+    const { page } = await searchParams;
+    console.log("page param:", page);
+    const response = await blogService.getBlogPosts({ page });
+    const posts = response.data?.data || [];
+    const pagination = response.data?.pagination || {
+        limit: 10,
+        page: 1,
+        total: 0,
+        totalPages: 1,
+    };
 
     return (
         <div className="p-4">
             <h1>blog post history</h1>
-            <HistoryTable posts={posts}/>
+            <HistoryTable posts={posts} />
+            <PaginationControls meta={pagination}/>
         </div>
     );
 }
